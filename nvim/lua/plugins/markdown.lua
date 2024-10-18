6699dd6836ba78f9ccfaa4052c477f634e675a7d
omar-nafea <omarahmednafea248@gmail.com>
2024-10-18 15:23:07 +0300

    adding config files

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,23 @@
+-- markdown-preview plugin configuration
+vim.g.mkdp_auto_start = 0
+vim.g.mkdp_auto_close = 1
+vim.g.mkdp_refresh_slow = 0
+vim.g.mkdp_command_for_global = 0
+vim.g.mkdp_open_to_the_world = 0
+vim.g.mkdp_open_ip = ""
+vim.g.mkdp_browser = "firefox"
+vim.g.mkdp_echo_preview_url = 0
+vim.g.mkdp_browserfunc = ""
+vim.g.mkdp_preview_options = {
+	mkit = {},
+	katex = {},
+	uml = {},
+	maid = {},
+	disable_sync_scroll = 0,
+	sync_scroll_type = "middle",
+	hide_yaml_meta = 1,
+	sequence_diagrams = {},
+	flowchart_diagrams = {},
+	content_editable = false,
+	disable_filename = 0,
+}
